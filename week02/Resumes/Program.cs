fbcf22ae30f7b2b0e534626253510f1d6e2b9792
AzureFirehawk <madison.thomas094@gmail.com>
2025-01-13 15:16:13 -0600

    Resume Project: Added Job and Resume classes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        job1._company = "Apple";
+        job1._jobTitle = "Software Developer";
+        job1._startYear = 2016;
+        job1._endYear = 2023;
+
+        job2._jobTitle = "Grand Poobah";
+        job2._company = "Some Such Place";
+        job2._endYear = 2025;
+        job2._startYear = 2014;
+
+        Resume resume = new Resume();
+        resume._name = "Some Person Here";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.ShowResume();
     }
 }
