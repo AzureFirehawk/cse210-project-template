15038e95f64e638586c899d8791bf6a5da39a59b
AzureFirehawk <madison.thomas094@gmail.com>
2025-02-03 19:17:38 -0600

    Week 5 Inheritance practice

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine($"\n{mathAssignment.GetHomeworkList()}");
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Cause of World War II");
+        Console.WriteLine($"\n{writingAssignment.GetWritingInformation()}");
+
     }
 }
