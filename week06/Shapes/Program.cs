b6160a0db734d1bea43874b8ad0421dcee21fa0a
AzureFirehawk <madison.thomas094@gmail.com>
2025-02-10 20:54:20 -0600

    polymorphism practice activity commit

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+        Square square = new Square("red", 5.0);
+        Rectangle rectangle = new Rectangle("blue", 4.0, 6.0);
+        Circle circle = new Circle("green", 3.0);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);    
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetColor()} {shape.GetArea()}");
+        }
+        
     }
 }
