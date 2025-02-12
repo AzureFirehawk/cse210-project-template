8642458b9e027174adb9dda60b8f45d4619d2f7a
AzureFirehawk <madison.thomas094@gmail.com>
2025-02-12 13:13:12 -0600

    Eternal Quest classes initial commit

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,24 @@
+public abstract class Goal
+{
+    protected string _name;
+    protected string _description;
+    protected int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _name = name;
+        _description = description;
+        _points = points;
+    }
+
+    public abstract void RecordEvent();
+
+    public abstract bool IsComplete();
+
+    public virtual string GetDetailsString()
+    {
+        return $"[{IsComplete()}]{_name}: {_description}";   
+    }
+
+    public abstract void GetStringRepresentation();
+}
